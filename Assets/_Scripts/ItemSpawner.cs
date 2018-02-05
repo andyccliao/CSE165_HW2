52c3272b54896bbb08d02a0c267f3ea9c8dc458e
Carlos A Chulo <cchulo@UCSD.EDU>
2018-02-04 19:07:13 -0800

    simplified player state so that it is more readable, and all scripts can be  easily switched on/off

---
+++ Assets/_Scripts/ItemSpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+
+	public GameObject[] itemsToSpawn;
+	private GameObject itemSpawnPreview;
+	int index = 0;
+	public ItemManager itemManager;
+	public GameObject leftControllerRef;
+	public GameObject rightControllerRef;
+
+
+	void Awake(){
+		if (itemsToSpawn.Length > 0) {
+			for (var i = 0; i < itemsToSpawn.Length; i++) {
+				itemsToSpawn [i].SetActive (false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (itemsToSpawn.Length > 0) {
+			//select itemSpawn Preview
+			itemSpawnPreview = itemsToSpawn [index];
+
+			//detect whether or not the user wants to view next or previous item
+			if (OVRInput.GetDown (OVRInput.Button.One)) {
+				index = (index - 1 + itemsToSpawn.Length) % itemsToSpawn.Length;
+				itemSpawnPreview.SetActive (false);
+				itemSpawnPreview = itemsToSpawn [index];
+			} else if (OVRInput.GetDown (OVRInput.Button.Two)) {
+				index = (index + 1) % itemsToSpawn.Length;
+				itemSpawnPreview.SetActive (false);
+				itemSpawnPreview = itemsToSpawn [index];
+			}
+			//set the current previewed item active
+			itemSpawnPreview.SetActive (true);
+
+			// manipulate preview to be in position of where we want item to spawn
+			RaycastHit hit;
+			//				if (lineRenderComponent != null && 
+			if (rightControllerRef != null &&
+			  Physics.Raycast (rightControllerRef.transform.position, rightControllerRef.transform.forward, out hit)) {
+
+				if (Vector3.Distance (rightControllerRef.transform.position, hit.point) <= 5.0f) {
+					itemSpawnPreview.transform.position = new Vector3 (
+						hit.point.x, 
+						itemSpawnPreview.transform.position.y,
+						hit.point.z
+					);
+
+					var itemPreviewState = itemSpawnPreview.GetComponent<ItemPreviewState> ();
+					if (itemPreviewState != null) {
+						if (itemPreviewState.CanBePlaced) {
+							if (OVRInput.GetDown (OVRInput.Button.SecondaryThumbstick)) {
+								itemManager.SpawnObject (itemSpawnPreview, itemPreviewState.transform.position);
+							} 
+						}
+					}
+				}
+			}
+		}
+	}
+
+	void OnDisable() {
+		setItemSpawnPreviewActive (false);
+	}
+
+
+	void setItemSpawnPreviewActive(bool flag){
+		if (itemSpawnPreview != null) {
+			itemSpawnPreview.SetActive (flag);
+		}
+	}
+}
--- Assets/_Scripts/PlayerState.cs
+++ Assets/_Scripts/PlayerState.cs
@@ -3,147 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(ItemSpawner))]
+[RequireComponent(typeof(Teleport))]
 public class PlayerState : MonoBehaviour {
 
-	public bool isSelecting = false;
+//	public bool isSelecting = false;
 	public Teleport teleportRef;
-	public bool spawnMode = false;
-	public bool rayCast = true;
-	public bool gogoHands = false;
+//	public bool spawnMode = false;
+//	public bool rayCast = true;
+//	public bool gogoHands = false;
 //	public GameObject lineRendererGobject;
 //	private LineRenderer lineRenderComponent;
-	public GameObject leftControllerRef;
-	public GameObject rightControllerRef;
-	public ItemManager itemManager;
 
-	public GameObject[] itemsToSpawn;
-	int index = 0;
-
-	private GameObject itemSpawnPreview;
+	private ItemSpawner itemSpawner;
 
 	public Text textComponent;
 
-	void Awake(){
-		if (itemsToSpawn.Length > 0) {
-			for (var i = 0; i < itemsToSpawn.Length; i++) {
-				itemsToSpawn [i].SetActive (false);
-			}
-		}
-	}
+	private EPlayerStates playerState;
+
+	enum EPlayerStates {
+		Teleport = 0,
+		Spawn = 1,
+		Select1 = 2,
+		Select2 = 3,
+		MeasuringTool = 4,
+		SIZE = 5 // MAKE SURE THIS IS AT THE END
+	};
 
 	void Start(){
+		playerState = EPlayerStates.Teleport;
 //		lineRenderComponent = lineRendererGobject.GetComponent<LineRenderer> ();
+		itemSpawner = GetComponent<ItemSpawner>();
+		itemSpawner.enabled = false;
 	}
 	// Update is called once per frame
 	void Update () {
 
 		// tap Y to toggle between selection mode and teleport mode
 		if (OVRInput.GetDown (OVRInput.Button.Four)) {
-			isSelecting = !isSelecting;
-			Debug.Log (isSelecting);
+			playerState = (EPlayerStates)(((int)(((int)playerState) + 1)) % ((int)EPlayerStates.SIZE));
 		}
 
-		// set teleport mode to on or off
-		if (teleportRef != null ){//&& lineRendererGobject != null) {
-//			lineRendererGobject.SetActive (!isSelecting);
-			teleportRef.enabled = !isSelecting;
+		switch (playerState) {
+		case EPlayerStates.Teleport:
+			teleportRef.enabled = true;
+			itemSpawner.enabled = false;
+			SetText ("Teleport Mode");
+			break;
+		case EPlayerStates.Spawn:
+			teleportRef.enabled = false;
+			itemSpawner.enabled = true;
+			SetText ("Spawn Mode");
+			break;
+		case EPlayerStates.Select1:
+			teleportRef.enabled = false;
+			itemSpawner.enabled = false;
+			SetText ("Select1 Mode");
+			break;
+		case EPlayerStates.Select2:
+			teleportRef.enabled = false;
+			itemSpawner.enabled = false;
+			SetText ("Select2 Mode");
+			break;
+		case EPlayerStates.MeasuringTool:
+			teleportRef.enabled = false;
+			itemSpawner.enabled = false;
+			SetText ("Measuring Mode");
+			break;
+		default:
+			teleportRef.enabled = true;
+			itemSpawner.enabled = false;
+			SetText ("Teleport Mode");
+			break;
 		}
-
-		//if we are selecting
-		if (isSelecting) {
-
-			// tap X to toggle between spawn mode and regular selection
-			if (OVRInput.GetDown (OVRInput.Button.Three)) {
-				spawnMode = !spawnMode;
-			}
-				
-			//if we are not in spawn mode
-			if (!spawnMode) {
-				// hide the spawn preview
-				setItemSpawnPreviewActive (false);
-
-				// tap A to select RayCast Selection Mode
-				if (OVRInput.GetDown (OVRInput.Button.One)) {
-					rayCast = true;
-					gogoHands = false;
-				// tap B to select gogoHands Selection Mode
-				} else if (OVRInput.GetDown (OVRInput.Button.Two)) {
-					rayCast = false;
-					gogoHands = true;
-				}
-			}
-
-			// if we are in spawn mode
-			if (spawnMode && itemsToSpawn.Length > 0) {
-				//select itemSpawn Preview
-				itemSpawnPreview = itemsToSpawn [index];
-
-				//detect whether or not the user wants to view next or previous item
-				if (OVRInput.GetDown (OVRInput.Button.One)) {
-					index = (index - 1 + itemsToSpawn.Length) % itemsToSpawn.Length;
-					itemSpawnPreview.SetActive (false);
-					itemSpawnPreview = itemsToSpawn [index];
-				} else if (OVRInput.GetDown (OVRInput.Button.Two)) {
-					index = (index + 1) % itemsToSpawn.Length;
-					itemSpawnPreview.SetActive (false);
-					itemSpawnPreview = itemsToSpawn [index];
-				}
-				//set the current previewed item active
-				itemSpawnPreview.SetActive (true);
-
-				// manipulate preview to be in position of where we want item to spawn
-				RaycastHit hit;
-//				if (lineRenderComponent != null && 
-				if(	rightControllerRef != null && 
-					Physics.Raycast (rightControllerRef.transform.position, rightControllerRef.transform.forward, out hit)) {
-
-					if (Vector3.Distance (rightControllerRef.transform.position, hit.point) <= 5.0f) {
-						itemSpawnPreview.transform.position = new Vector3 (
-							hit.point.x, 
-							itemSpawnPreview.transform.position.y,
-							hit.point.z
-						);
-
-						var itemPreviewState = itemSpawnPreview.GetComponent<ItemPreviewState> ();
-						if (itemPreviewState != null) {
-							if (itemPreviewState.CanBePlaced) {
-								if (OVRInput.GetDown (OVRInput.Button.SecondaryThumbstick)) {
-									itemManager.SpawnObject (itemSpawnPreview, itemPreviewState.transform.position);
-								} 
-							}
-						}
-					}
-				}
-			} 
-				
-		} else {
-			setItemSpawnPreviewActive (false);
-		}
-
-		SetText ();
+			
 	}
 
-	void setItemSpawnPreviewActive(bool flag){
-		if (itemSpawnPreview != null) {
-			itemSpawnPreview.SetActive (flag);
-		}
-	}
 
-	void SetText(){
+	void SetText(string text){
 		if (textComponent != null) {
-			if (!isSelecting) {
-				textComponent.text = "Teleport Mode";
-			} else {
-				if (!spawnMode) {
-					if (rayCast) {
-						textComponent.text = "RayCast Select";
-					} else if (gogoHands) {
-						textComponent.text = "GoGo-Hands Select";
-					}
-				} else if (spawnMode) {
-					textComponent.text = "Spawn Mode";
-				}
-			}
+			textComponent.text = text;
 		}
 	}
 }
